b4d19f24973925db725be1511018043de58d5624
Bharada Bharat <bhardabharat21@gmail.com>
2025-06-07 22:48:52 +0530

    my portfolio

---
+++ My portfolio/intro.js
@@ -0,0 +1,9 @@
+function turnOn() {
+  const bulb = document.getElementById("bulb");
+  bulb.classList.add("glow");
+}
+
+function turnOff() {
+  const bulb = document.getElementById("bulb");
+  bulb.classList.remove("glow");
+}
